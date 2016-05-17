42b786e0006268f15879cddd05b2f99ffb83eb17
copley <qcopley@gmail.com>
2016-05-17 15:30:15 +1200

    changes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,13 @@
 var express = require('express');
 var multer = require('multer');
 var router = express.Router();
-var pg = require('pg').native;
+var pg = require('pg');
 
-// Original connection method. Date:  21:21 12/05/2016 
-//
 var connectionString = "postgres://swen303group7:1234567890@marketplace.cl3zdftaq5q4.ap-southeast-2.rds.amazonaws.com:5432/marketplace"
-
-
-/*pg.connect(connectionString, function(err, client, done){
-    if(err){
-        done();
-        console.log(err);
-        return;
-    }
-    var query = client.query('SELECT * FROM users');
-    var rows = [];
-    query.on('row', function(row){
-        rows.push(row);
-    });
-    query.on('end', function(){
-        console.log('The solution is: ', rows[0]);
-        done();
-        return;
-    });
-
-});*/
-
-//connection.end();
 
 router.get("/",function(req,res) {
     res.render('index', {title: 'Top End Code'});
 });
-
-
 
 router.get('/test_database', function(request, response) {
     pg.connect(connectionString, function(err, client, done){
